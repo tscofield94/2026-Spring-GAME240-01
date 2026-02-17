fe5323103f7beeeadf0c1a43148b466691f1ce31
Thien Scofield <scofield@chapman.edu>
2026-02-16 22:39:47 -0800

    first assignment
    typin stuff, etc etc. cant pronounce own name and whatnot

---
+++ homework/february_17/project/ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, my name is Thien (pronounced T-N... I think....)! But if that's too hard, you can just call me Kevin!");
