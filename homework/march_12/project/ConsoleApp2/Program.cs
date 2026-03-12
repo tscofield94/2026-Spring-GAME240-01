47574c8c5ace5b1dd1640450a73e4de6ef9bc9e3
Thien Scofield <scofield@chapman.edu>
2026-03-12 14:35:44 -0700

    hw 4
    maxmin

---
+++ homework/march_12/project/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = [123, 5, 0, -10, 7];
+
+int maxNum, minNum;
+
+int i = 0;
+
+maxNum = numbers[0];
+minNum = numbers[0];
+while (i < numbers.Length)
+{
+    if (numbers[i] > maxNum)
+    {
+        maxNum = numbers[i];
+    }
+    if (numbers[i] < minNum)
+    {
+        minNum = numbers[i];
+    }
+
+    i++;
+}
+Console.WriteLine("Largest number is " + maxNum);
+Console.WriteLine("Smallest number is " + minNum);
