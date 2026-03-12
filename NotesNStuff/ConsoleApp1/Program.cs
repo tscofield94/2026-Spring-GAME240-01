05a1ecbaec0ea13f3d5b9d97adac60b98401e7d5
Thien Scofield <scofield@chapman.edu>
2026-03-12 14:29:21 -0700

    note folder

---
+++ NotesNStuff/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// int a = 10;
+// int b = 99;
+//
+// int tempSwap = a;
+// a = b;
+// b = tempSwap;
+//
+// Console.WriteLine("A is now " + a);
+// Console.WriteLine("B is now " +b);
+
+// int[] numbers = [123, 5, 0, -10, 7];
+//
+// int maxNum, minNum;
+//
+// int i = 0;
+//
+// maxNum = numbers[0];
+// minNum = numbers[0];
+// while (i < numbers.Length)
+// {
+//     if (numbers[i] > maxNum)
+//     {
+//         maxNum = numbers[i];
+//     }
+//     if (numbers[i] < minNum)
+//     {
+//         minNum = numbers[i];
+//     }
+//
+//     i++;
+// }
+// Console.WriteLine("Largest number is " + maxNum);
+// Console.WriteLine("Smallest number is " + minNum);
+
+int[] numArray = new int[10];
+Console.WriteLine(numArray[2]);
